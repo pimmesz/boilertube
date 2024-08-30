ae54218de91877c64557a5810472356a0a86a9b4
Pim Zonneveld <pim.zonneveld@capgemini.com>
2024-08-30 18:41:48 +0200

    feat(DPCWEB-0000): add subdomain support

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,8 +5,8 @@
 			:href="`https://${channel.subdomain}.tube.yt`"
 			v-for="channel in availableChannels" :key="channel.id"
 		>
-			<h1>{{ channel.channelName }}</h1>
-			<img v-if="channel.thumbnails.high.url" :src="channel.thumbnails.high.url" alt="Channel logo" />
+			<h1>{{ channel?.channelName }}</h1>
+			<img v-if="channel?.thumbnails?.high?.url" :src="channel.thumbnails.high.url" alt="Channel logo" />
 		</a>
 	</div>
 </template>
