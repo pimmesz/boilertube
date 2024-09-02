e303c2b04dda1fd34f0a0fce6ae6fba55b55c672
Pim Zonneveld <pim.zonneveld@capgemini.com>
2024-09-02 20:11:14 +0200

    feat(DPCWEB-0000): fix live check

--- api/index.js
+++ api/index.js
@@ -15,11 +15,6 @@ const app = express();
 const port = process.env.PORT || 3003;
 
 app.use(express.json());
-app.use(cors({
-  origin: ['https://tube.yt', 'https://www.tube.yt', 'http://localhost:3000'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
 
 dotenv.config();
 
