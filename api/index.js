a766d653fb36541e3a5a5d8e86d1b7744f349bff
Pim Zonneveld <pim.zonneveld@capgemini.com>
2024-09-12 11:11:46 +0200

    feat(DPCWEB-0000): fix oauth

--- api/index.js
+++ api/index.js
@@ -448,7 +448,7 @@ app.get('/oauth2callback', async (req, res) => {
     process.env.CLIENT_REFRESH_TOKEN = tokens.refresh_token;
     process.env.CLIENT_EXPIRATION_DATE = tokens.expiry_date;
 
-    res.send('Authentication successful! You can close this window.');
+    res.send('Authentication successful! You can close this window.', tokens);
   } catch (error) {
     console.error('Error with OAuth callback:', error);
     res.status(500).send('Authentication failed: ' + error.message);
